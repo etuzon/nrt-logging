0f95ed5f1865700e44295ed9a5ec6961b3763df2
Eyal Tuzon <eyal.tuzon.dev@gmail.com>
2022-11-11 18:20:19 +0200

    version 1.2.1

--- nrt_logging/__init__.py
+++ nrt_logging/__init__.py
@@ -1,10 +1,9 @@
 """
-Version 1.1.0: Add support for configuration file.
+Version 1.1.0: Add support for a configuration file.
 Version 1.2.0:
     Add support for:
         Add the same Stream Handler in multiple loggers.
-        Limit logger file size, split and zip log files.
-
+        Limit logger file size, split, and zip log files.
 """
 
-__version__ = '1.2.0'
+__version__ = '1.2.1'
